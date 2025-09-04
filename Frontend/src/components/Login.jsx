d81ebac8de32f248bc5dd54036d34a0218023be4
Anshul Sharma <118748592+NameIsAnsh@users.noreply.github.com>
2025-09-04 14:10:01 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .post("https://fullstackproject-anfn.onrender.com/", userInfo)
+      .post("https://fullstackproject-anfn.onrender.com/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
